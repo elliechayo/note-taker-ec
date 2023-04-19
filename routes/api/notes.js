ee986b6e5d16386fe6e3cd32a735436f9a9d325a
Elinor Chayo <elliechayo@gmail.com>
2023-04-19 14:21:33 -0400

    updated the api/notes.js with code to called on all of node require paths as well as connected to the db file to be able to update file with new data

---
+++ routes/api/notes.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+const shortid = require("shortid");
+const fs = require("fs");
+const path = require("path");
+
+let db = require("../../db/db.json");
+
+const updateFile = (data) => {
+  // get the file location
+  const fileLoc = path.join(__dirname, "..", "..", "/db/db.json");
+  // write the new data to the file
+  fs.writeFileSync(fileLoc, data);
+};
+
+// method GET
+// route /api/notes/
+// method public
+// returns the json data from the local JSON file
+router.get("/", (req, res) => {
+     res.json(db);
+   });
